676aeffa6354e828e1e81ba67357ce3a122d4d57
Mark LeMoine <linclelinkpart5@gmail.com>
2020-05-15 20:26:18 -0700

    Solidifying API and deciding on in-place and chainable function names

--- src/lib.rs
+++ src/lib.rs
@@ -39,14 +39,14 @@ pub struct SpreadsheetView<D: Display + Ord> {
 }
 
 impl<D: Display + Ord> Default for SpreadsheetView<D> {
-    /// Creates a new empty `SpreadsheetView` without any columns.
+    /// Creates a new empty view without any columns.
     fn default() -> Self {
         Self::new()
     }
 }
 
 impl<D: Display + Ord> SpreadsheetView<D> {
-    /// Creates a new empty `SpreadsheetView` without any columns.
+    /// Creates a new empty view without any columns.
     pub fn new() -> Self {
         Self {
             columns: IndexMap::new(),
@@ -67,41 +67,75 @@ impl<D: Display + Ord> SpreadsheetView<D> {
         }
     }
 
-    /// Adds a column to this `SpreadsheetView`. This includes a key value along
-    /// with a title for visual display.
+    /// Adds a column to this view. This includes a key value along with a title
+    /// for visual display.
     pub fn add_column(&mut self, key: String, title: String) {
         self.columns.insert(key, title);
     }
 
-    /// Removes a column from this `SpreadsheetView`.
-    pub fn remove_column(&mut self, key: &str) {
-        self.columns.remove(key);
-    }
-
-    /// Adds a record to this `SpreadsheetView`. This will be inserted as the
-    /// last record.
-    pub fn add_record(&mut self, record: Record<D>) {
-        self.records.push(record)
-    }
-
-    /// Removes a record from this `SpreadsheetView`.
-    pub fn remove_record(&mut self, index: usize) -> Option<Record<D>> {
-        if index < self.records.len() { Some(self.records.remove(index)) }
-        else { None }
-    }
-
-    /// Clears all records from this `SpreadsheetView`.
-    pub fn clear_records(&mut self) {
-        self.records.clear();
-    }
-
-    /// Chainable variant of `add_column`.
+    /// Chainable version of `add_column`.
     pub fn with_column(&mut self, key: String, title: String) -> &mut Self {
         self.add_column(key, title);
         self
     }
 
-    /// Sorts the records in this `SpreadsheetView` by the specified column.
+    /// Removes a column from this view.
+    pub fn remove_column(&mut self, key: &str) {
+        self.columns.remove(key);
+    }
+
+    /// Appends a record to the end of this view.
+    pub fn push_record(&mut self, record: Record<D>) {
+        self.records.push(record)
+    }
+
+    /// Chainable version of `push_record`.
+    pub fn with_record(&mut self, record: Record<D>) -> &mut Self {
+        self.push_record(record);
+        self
+    }
+
+    /// Extends this view with the contents of an iterator containing records.
+    pub fn extend_records<I>(&mut self, iter: I)
+    where
+        I: IntoIterator<Item = Record<D>>,
+    {
+        self.records.extend(iter);
+    }
+
+    /// Chainable version of `extend_records`.
+    pub fn with_records<I>(&mut self, iter: I) -> &mut Self
+    where
+        I: IntoIterator<Item = Record<D>>,
+    {
+        self.extend_records(iter);
+        self
+    }
+
+    /// Removes and returns the last record from this view, or `None` if there
+    /// are no records.
+    pub fn pop_record(&mut self) -> Option<Record<D>> {
+        self.records.pop()
+    }
+
+    /// Removes and returns the record at a specified index from this view, or
+    /// `None` if the index is out of bounds.
+    pub fn remove_record(&mut self, index: usize) -> Option<Record<D>> {
+        if index < self.records.len() { Some(self.records.remove(index)) }
+        else { None }
+    }
+
+    /// Clears all records from this view.
+    pub fn clear_records(&mut self) {
+        self.records.clear();
+    }
+
+    /// Returns the number of records in this view.
+    pub fn len_records(&self) -> usize {
+        self.records.len()
+    }
+
+    /// Sorts the records in this view by the specified column.
     /// This sort is stable, so multiple calls of this method with different
     /// columns will co-sort as expected.
     pub fn sort_rows(&mut self, key: &str, ascending: bool) {
